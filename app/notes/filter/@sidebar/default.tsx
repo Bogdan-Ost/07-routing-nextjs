36a97d89b2cfda30c8e8086fcf044a230e924315
Ostapenko Bogdan <bogdan.ostapenko.97@gmail.com>
2026-02-22 20:58:19 +0200

    asd

--- app/notes/filter/@sidebar/default.tsx
+++ app/notes/filter/@sidebar/default.tsx
@@ -6,13 +6,16 @@ const TAGS = ["All", "Todo", "Work", "Personal", "Meeting", "Shopping"];
 export default function SidebarNotes() {
   return (
     <ul className={css.menuList}>
-      {TAGS.map((tag) => (
-        <li key={tag} className={css.menuItem}>
-          <Link href={`/notes/filter/${tag}`} className={css.menuLink}>
-            {tag}
-          </Link>
-        </li>
-      ))}
+      {TAGS.map((tag) => {
+        const path = tag === "All" ? "all" : tag;
+        return (
+          <li key={tag} className={css.menuItem}>
+            <Link href={`/notes/filter/${path}`} className={css.menuLink}>
+              {tag === "All" ? "All notes" : tag}
+            </Link>
+          </li>
+        );
+      })}
     </ul>
   );
 }
